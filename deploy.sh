44b4ca03e1033fa4070717c154c69a459ac557a0
Tobia Ochsner <tobia.ochsner@hotmail.com>
2022-02-21 21:26:56 +0100

    Test

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,6 @@
 echo -n "Please enter a build message:"
 read message
 
-git clone https://github.com/tochsner/smart-mirror-deployment.git
-
-cd smart-mirror-display
 npx sng build
 
 xcopy dist\smart-mirror-display ..\smart-mirror-deployment\.
@@ -12,6 +9,3 @@ cd ..\smart-mirror-deployment
 git add .
 git commit -m "$message"
 git push
-
-cd ..
-del smart-mirror-deployment /q
