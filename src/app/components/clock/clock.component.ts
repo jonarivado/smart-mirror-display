4d3c1884b2b1ed675a5ef52b63bbef0acc3fcfc7
Tobia Ochsner <tobia.ochsner@hotmail.com>
2022-02-22 21:09:08 +0100

    Test

--- src/app/components/clock/clock.component.ts
+++ src/app/components/clock/clock.component.ts
@@ -12,14 +12,7 @@ export class ClockComponent implements OnInit {
   data: any;
 
   refreshTime() {
-    let now: Date = new Date();
-    let hours = now.getHours();
-    let minutes = now.getMinutes();
-    let seconds = now.getSeconds();
-    let secondsString = seconds < 10 ? '0' + seconds : seconds;
-    let minutesString = minutes < 10 ? '0' + minutes : minutes;
-    this.time = hours + ':' + minutesString + ':' + secondsString;
-    this.data = [];
+    
   }
 
   ngAfterViewInit() {
